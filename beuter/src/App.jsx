2ff4afd9994f2667d4ccf54845240a189f64b616
Nhan Nguyen <nhannguyen@Nhans-iMac.local>
2020-07-24 15:13:57 -0500

    nav-bar

---
+++ beuter/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SideNav from "./Components/SideNav";
+
+function App() {
+  return (
+    <div className="App">
+      <SideNav />
+    </div>
+  );
+}
+
+export default App;
