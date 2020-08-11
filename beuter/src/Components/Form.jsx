eaf44a39b11676484ba260199f375410ac199e17
Nhan Nguyen <nhannguyen@Nhans-iMac.local>
2020-08-11 14:27:28 -0500

    update form

--- beuter/src/Components/Form.jsx
+++ beuter/src/Components/Form.jsx
@@ -1,6 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { navigate } from "@reach/router";
+import styled from "styled-components";
+
+const DivStyled = styled.div``;
+
+const InputStyled = styled.input`
+  font-family: system-ui;
+  font-size: 12px;
+`;
+
+const FormStyled = styled.form``;
+
+const ButtonCreate = styled.button``;
+
+const LabelStyled = styled.label``;
 
 const Form = (props) => {
   const [title, setTitle] = useState("");
@@ -56,226 +70,225 @@ const Form = (props) => {
   };
 
   return (
-    <div className="container" style={{ marginLeft: "400px" }}>
-      <h1>Form</h1>
-      <form onSubmit={addProduct}>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+    <DivStyled className="container" style={{ marginLeft: "400px" }}>
+      <FormStyled onSubmit={addProduct}>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Title
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setTitle(e.target.value)}
               value={title}
             />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+          </DivStyled>
+        </DivStyled>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Title URL
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setTitleUrl(e.target.value)}
               value={title_url}
             />
-          </div>
-        </div>
+          </DivStyled>
+        </DivStyled>
 
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Price
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="number"
               onChange={(e) => setPrice(e.target.value)}
               value={price}
               placeholder="$"
             />
-          </div>
-        </div>
+          </DivStyled>
+        </DivStyled>
 
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Description 1
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setDescription1(e.target.value)}
               value={description1}
             />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+          </DivStyled>
+        </DivStyled>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Description 2
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setDescription2(e.target.value)}
               value={description2}
             />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+          </DivStyled>
+        </DivStyled>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Description 3
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setDescription3(e.target.value)}
               value={description3}
             />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+          </DivStyled>
+        </DivStyled>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Description 4
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setDescription4(e.target.value)}
               value={description4}
             />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+          </DivStyled>
+        </DivStyled>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Description 5
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setDescription5(e.target.value)}
               value={description5}
             />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+          </DivStyled>
+        </DivStyled>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Img Url 1
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setImgUrl1(e.target.value)}
               value={img_url1}
             />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+          </DivStyled>
+        </DivStyled>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Img Url 2
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setImgUrl2(e.target.value)}
               value={img_url2}
             />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+          </DivStyled>
+        </DivStyled>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Img Url 3
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setImgUrl3(e.target.value)}
               value={img_url3}
             />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+          </DivStyled>
+        </DivStyled>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Img Url 4
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setImgUrl4(e.target.value)}
               value={img_url4}
             />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+          </DivStyled>
+        </DivStyled>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Size
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setSize(e.target.value)}
               value={size}
             />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+          </DivStyled>
+        </DivStyled>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Size 2
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setSize2(e.target.value)}
               value={size2}
             />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+          </DivStyled>
+        </DivStyled>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Fit
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setFit(e.target.value)}
               value={fit}
             />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+          </DivStyled>
+        </DivStyled>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Fit 2
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setFit2(e.target.value)}
               value={fit2}
             />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="title" className="col-sm-2 col-form-label">
+          </DivStyled>
+        </DivStyled>
+        <DivStyled className="form-group row">
+          <LabelStyled htmlFor="title" className="col-sm-2 col-form-label">
             Category
-          </label>
-          <div className="col-sm-10">
-            <input
+          </LabelStyled>
+          <DivStyled className="col-sm-10">
+            <InputStyled
               type="text"
               onChange={(e) => setCategory(e.target.value)}
               value={category}
             />
-          </div>
-        </div>
+          </DivStyled>
+        </DivStyled>
 
-        <div className="form-group row">
-          <div className="col-sm-10 offset-sm-2">
-            <button type="submit" className="btn btn-primary">
+        <DivStyled className="form-group row">
+          <DivStyled className="col-sm-10 offset-sm-2">
+            <ButtonCreate type="submit" className="btn btn-primary">
               Create
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
+            </ButtonCreate>
+          </DivStyled>
+        </DivStyled>
+      </FormStyled>
+    </DivStyled>
   );
 };
 export default Form;
