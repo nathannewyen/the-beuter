ea9f64bcb29ed02e2ba81a2e91a9ebb0aa91acea
Nhan Nguyen <nhannguyen@Nhans-iMac.local>
2020-08-11 13:57:29 -0500

    first deploy

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express"),
     app = express(),
     cors = require("cors"),
-    port = 8000,
+    port = process.env.PORT || 8000,
     db = "beuter",
+    path = require("path"),
     server = app.listen(port, () => console.log(`Listening to on port ${port}`));
 
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('beuter/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'beuter', 'build', 'index.html'));
+    })
+}
 
 require("./server/config/database.config")(db);
 require("./server/routes/product.route")(app);
