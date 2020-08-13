874b817f6192e00a294f74c66105cda83a11a8d0
Nhan Nguyen <nathan13574@gmail.com>
2020-08-13 00:25:05 -0500

    update

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ const express = require("express"),
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('beuter/build'))
+
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname + '/client/build/index.html'));
     });
-};
+}
 require("./server/config/database.config")(db);
 require("./server/routes/product.route")(app);
