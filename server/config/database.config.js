a44a95dcbc774959121b7f4d69e770efdaa1dae2
Nhan Nguyen <nathan13574@gmail.com>
2020-08-12 20:21:24 -0500

    update database

--- server/config/database.config.js
+++ server/config/database.config.js
@@ -5,7 +5,6 @@ module.exports = (name) => {
         .connect(process.env.MONGODB_URI || `mongodb://localhost/${name}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: false
         })
         .then(() => console.log(`Successfully connected to ${name}`))
         .catch((err) => console.log(err));
