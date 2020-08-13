33e5bbf89fd0466163aa22e763afcef0d364b431
Nhan Nguyen <nathan13574@gmail.com>
2020-08-13 14:54:24 -0500

    update

--- server.js
+++ server.js
@@ -3,18 +3,20 @@ const express = require("express"),
     cors = require("cors"),
     port = process.env.PORT || 8000,
     db = "beuter",
-    path = require("path"),
-    server = app.listen(port, () => console.log(`Listening to on port ${port}`));
+    path = require("path")
 
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(__dirname, 'beuter/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/beuter/build/index.html'));
-    });
-}
 require("./server/config/database.config")(db);
 require("./server/routes/product.route")(app);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('beuter/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname + '/beuter/build/index.html'));
+    });
+}
+
+app.listen(port, () => console.log(`Listening to on port ${port}`));
