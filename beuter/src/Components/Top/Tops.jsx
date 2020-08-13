393f4f82f2e1fd607da5f7ed9cd3921595144eff
Nhan Nguyen <nathan13574@gmail.com>
2020-08-13 02:04:18 -0500

    update route and heroku

--- beuter/src/Components/Top/Tops.jsx
+++ beuter/src/Components/Top/Tops.jsx
@@ -23,6 +23,7 @@ const Tops = (props) => {
       setLoading(true);
       const res = await axios.get("/api/products");
       setProducts(res.data);
+      console.log(res.data)
       setLoading(false);
     };
     document.title = `Shop - The Beuter`;
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static('beuter/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/beuter/build/index.html'));
+        res.sendFile(path.join(__dirname + '/client/build/index.html'));
     });
 }
 require("./server/config/database.config")(db);
